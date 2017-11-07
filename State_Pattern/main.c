161ecc9d9c09b77e40663de0696e7316b95ba09f
Seventeen <310634231@qq.com>
2017-11-08 00:30:58 +0800

    state pattern
    state pattern

--- State_Pattern/main.c
+++ State_Pattern/main.c
@@ -1,7 +1,5 @@
 
-#include "singleton.h"
-#include <stdio.h>
-
+#include "state_pattern.h"
 
 int main(void)
 {
@@ -9,24 +7,18 @@ int main(void)
     return 0;
 }
 
-//test
-//notice:对于单线程 单例模式不需要考虑线程之间的竞争
-//       对于多线程 需要考虑同步同一时间拿到唯一一个对象
-//       针对一次获取对象 分为 懒汉式 饿汉式
-//       懒汉式   拿对象才生成
-//       饿汉式   已经初始化好
+//notice:取代if 或者 switch
+//概念： this state to do something
+// this room state to do XXX
 void run()
 {
-    User *user = getUserInstance();  //noly one
-    User *user2 = getUserInstance();
-    printf("----------user1-----------\n");
-    user->about(user);
-    printf("----------user2-----------\n");
-    user2->about(user2);
-    //change user 1
-    strcpy(user->address,"home");
-    strcpy(user->phone,"17");
-    printf("----------user2-----------\n");
-    user2->about(user2);
-    printf("---------------------\n");
+    Room *room = CreatRoom();
+    room->bookRoom(room);
+    room->unsubscribeRoom(room);
+    room->checkInRoom(room);
+    room->checkOutRoom(room);
+    room->checkOutRoom(room);
+    room->checkInRoom(room);
+    room->unsubscribeRoom(room);
+    room->bookRoom(room);
 }
